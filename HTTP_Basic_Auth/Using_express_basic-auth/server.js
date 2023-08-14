331d9849cff1778d730a3fd83e7f5615289eb9c5
Alecky3 <alexmuthini3@gmail.com>
2023-08-14 14:41:57 +0300

    added basic auth

--- HTTP_Basic_Auth/Using_express_basic-auth/server.js
+++ HTTP_Basic_Auth/Using_express_basic-auth/server.js
@@ -1,7 +1,28 @@
 import express from "express";
+import expressBasicAuth from "express-basic-auth";
+import path from "path"
 
 // main application
 const app = express();
+
+
+// middlewares
+app.use(expressBasicAuth({
+    users : {
+        'alex' : '123'
+    },
+    challenge: true,
+    realm: "Exress Basic auth"
+}))
+
+app.set("author","alex")
+
+// end points
+app.get('/', (req,res)=>{
+    
+    res.sendFile(path.resolve('.', 'HTML/home.html'))
+})
+
 
 
 // start server
---
+++ HTTP_Basic_Auth/Using_express_basic-auth/utils.js
@@ -0,0 +1,3 @@
+export function getUnauthorizedResponse(req) {
+    return req.auth()
+}
